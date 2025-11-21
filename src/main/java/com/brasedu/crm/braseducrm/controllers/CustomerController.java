bed52780853033deb259c5ece894cfe524972f33
Ryan <ryan.brito@grupoagtech.com1>
2025-11-21 11:09:47 -0300

    fix(customer): remover if from controller

--- src/main/java/com/brasedu/crm/braseducrm/controllers/CustomerController.java
+++ src/main/java/com/brasedu/crm/braseducrm/controllers/CustomerController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brasedu.crm.braseducrm.dto.CreateCustomerDto;
@@ -17,9 +16,10 @@ import com.brasedu.crm.braseducrm.services.CustomerService;
 
 import lombok.RequiredArgsConstructor;
 
+
 @RestController()
 @RequestMapping(value = "/customers")
-@RequiredArgsConstructor()
+@RequiredArgsConstructor
 public class CustomerController {
     private final CustomerService customerService;
 
