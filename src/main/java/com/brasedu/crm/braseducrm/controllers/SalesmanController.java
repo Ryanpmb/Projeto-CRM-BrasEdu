e521f2f8f04aae8c8485047eb6f5b1bb8dab7769
Ryan Pablo <ryb47223@gmail.com>
2025-12-02 19:43:32 -0300

    fix:fix

--- src/main/java/com/brasedu/crm/braseducrm/controllers/SalesmanController.java
+++ src/main/java/com/brasedu/crm/braseducrm/controllers/SalesmanController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +38,7 @@ public class SalesmanController {
         }
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<SalesmanEntity> updateCustomer(
             @RequestBody SalesmanEntity newSalesmanInformations,
             @PathVariable String id) {
