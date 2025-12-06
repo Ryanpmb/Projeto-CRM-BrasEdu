44e7c31b5dd76aeec748e1d6c3c777d884fed0f1
Ryan <ryan.brito@grupoagtech.com1>
2025-12-06 00:00:31 -0300

    fix: fix

--- src/main/java/com/brasedu/crm/braseducrm/services/ExportCustomerCourseCsvService.java
+++ src/main/java/com/brasedu/crm/braseducrm/services/ExportCustomerCourseCsvService.java
@@ -1,33 +1,44 @@
 package com.brasedu.crm.braseducrm.services;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
-import com.brasedu.crm.braseducrm.repositories.CustomerCourseViewRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ExportCustomerCourseCsvService {
-    private final CustomerCourseViewRepository repository;
+    private final JdbcTemplate jdbc;
 
     public byte[] gerarCsv() {
 
-        List<Object[]> data = repository.findAllFromView();
+        String sql = """
+                    SELECT
+                        [Código do Cliente] AS codigoCliente,
+                        [Nome] AS nome,
+                        [Data de Nascimento] AS dataNascimento,
+                        [Nome do Curso] AS nomeCurso,
+                        [Carga Horária do Curso] AS cargaHorariaCurso,
+                        [Valor do Curso] AS valorCurso
+                    FROM vw_customersAndCoursesOfInteress
+                """;
+
+        List<Map<String, Object>> rows = jdbc.queryForList(sql);
 
         StringBuilder sb = new StringBuilder();
 
         sb.append("codigoCliente,nome,dataNascimento,nomeCurso,cargaHorariaCurso,valorCurso\n");
 
-        for (Object[] row : data) {
-            sb.append(row[0]).append(",");
-            sb.append(row[1]).append(",");
-            sb.append(row[2]).append(",");
-            sb.append(row[3]).append(",");
-            sb.append(row[4]).append(",");
-            sb.append(row[5]).append("\n");
+        for (Map<String, Object> row : rows) {
+            sb.append(row.get("codigoCliente")).append(",");
+            sb.append(row.get("nome")).append(",");
+            sb.append(row.get("dataNascimento")).append(",");
+            sb.append(row.get("nomeCurso")).append(",");
+            sb.append(row.get("cargaHorariaCurso")).append(",");
+            sb.append(row.get("valorCurso")).append("\n");
         }
 
         return sb.toString().getBytes();
